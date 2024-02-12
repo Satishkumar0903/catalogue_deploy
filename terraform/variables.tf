b466257066819875cad01768979b42149f8468cc
unknown <bsatheesh779@gmail.com>
2024-02-12 21:37:50 +0530

    catalogue_deploy

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "catalogue"
+  }
+}
+
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "devops-practice.site"
+}
+
+variable app_version {
+  
+}
+variable "iam_instance_profile" {
+  default = "Ec2Roleforshellscript"
+}
